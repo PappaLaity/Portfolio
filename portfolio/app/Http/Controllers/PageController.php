8cfa3717aaa0e6d48da69072bd1875fb9fbec955
pappalaity <pappalaity@gmail.com>
2021-01-02 13:39:13 +0000

    route modification

--- portfolio/app/Http/Controllers/PageController.php
+++ portfolio/app/Http/Controllers/PageController.php
@@ -9,18 +9,18 @@ class PageController extends Controller
     //
 
     public function home(){
-        return view('home-p');  
+        return view('dashboard.home-p');  
     }
     public function resume(){
-        return view('resume');        
+        return view('dashboard.resume');        
     }
     public function service(){
-        return view('service');        
+        return view('dashboard.service');        
     }
     public function contact(){
-        return view('contact');        
+        return view('dashboard.contact');        
     }    
     public function login(){
-        return view('login');        
+        return view('dashboard.login');        
     }        
 }
