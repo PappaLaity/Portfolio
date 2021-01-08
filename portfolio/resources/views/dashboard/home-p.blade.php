9681b25897c474baab1cc9ed15529685511dcb1f
pappalaity <pappalaity@gmail.com>
2021-01-08 23:16:26 +0000

    HomePage -- home-p blade About

--- portfolio/resources/views/dashboard/home-p.blade.php
+++ portfolio/resources/views/dashboard/home-p.blade.php
@@ -6,7 +6,7 @@
 <div class="main_resume">
 
     <div class="resume_desc">
-        <h3 style="text-align: center;">Je suis un Full Stack Developper</h3>
+        <h3 class="resume_desc_title">Je suis un Full Stack Developper</h3>
         <br>
     </div>
     <div class="resume_filiale">
@@ -14,69 +14,50 @@
         <p>Téléphone: +221774919731 / +2213913601</p>
         <p>Mail: pappalaity@gmail.com</p>
 
-        <!--Twitter -LinkedIn - Facebook - Github-->
+        <a href="https://github.com/PappaLaity"><img class="resume_filiale_ref" src="{{asset('images/icon_link/github.png')}}" alt="Papa Laity Ndiaye Github"></a>
+        <a href="https://twitter.com/pappalaity"><img class="resume_filiale_ref" src="{{asset('images/icon_link/twitterl.png')}}" alt="Papa Laity Ndiaye Twitter"></a>
+        <a href="https://web.facebook.com/laisco1/"> <img class="resume_filiale_ref" src="{{asset('images/icon_link/fb.png')}}" alt="Papa Laity Ndiaye Facebook"></a>
+        <a href="https://www.linkedin.com/in/pappalaity/"><img class="resume_filiale_ref" src="{{asset('images/icon_link/linkedIn.png')}}" alt="Papa Laity Ndiaye LinkedIn"></a>
+
     </div>
 
     <div class="resume_domaines">
-        <img class="img_profil" src="{{asset('images/pp3.jpeg')}}" alt="Papa laity Ndiaye Profil photo">
-        <a href="https://www.linkedin.com/in/pappalaity/">linkedin<img class="resume_filiale_ref" src="public/img/icon link/linkedIn.png" alt=""></a>
-        <a href="https://github.com/PappaLaity">github<img class="resume_filiale_ref" src="public/img/icon link/github.png" alt=""></a>
-        <a href="https://twitter.com/pappalaity">twitter<img class="resume_filiale_ref" src="public/img/icon link/twitterl.png" alt=""></a>
-        <a href="https://web.facebook.com/laisco1/"> facebook<img class="resume_filiale_ref" src="public/img/icon link/fb.png" alt=""></a>
+        <img class="img_profil" src="{{asset('images/dev.png')}}" alt="Papa laity Ndiaye Profil photo">
+        <br>
+
     </div>
 </div>
+<br>
 <hr>
 <div class="main_what_I_do">
 
     <h3>Ce que je fais??</h3> <br>
 
-    <div class="wid_item">
-        <img class="wid_item_ref" src="public/img/icon link/webbb.png" alt=""> <span class="wid_item_name">Web</span>
-        <p>Desc</p>
+    <nav class="nav nav-tabs">
+        <a class="nav-item nav-link active ml-5 pl-5 pr-5" href="#p1" data-toggle="tab">
+            WEB <img class="wid_item_ref" src="{{asset('images/icon_link/webbb.png')}}" alt="">
+        </a>
+        <a class="nav-item nav-link pl-5 pr-5" href="#p2" data-toggle="tab">
+            MOBILE <img class="wid_item_ref" src="{{asset('images/icon_link/mob.jpeg')}}" alt="">
+        </a>
+        <a class="nav-item nav-link pl-5 pr-5" href="#p3" data-toggle="tab">
+            DATA SCIENCE <img class="wid_item_ref" src="{{asset('images/icon_link/ia.png')}}" alt="">
+        </a>
+        <a class="nav-item nav-link pl-5 pr-5" href="#p4" data-toggle="tab">
+            IOT <img class="wid_item_ref" src="{{asset('images/icon_link/wgh.png')}}" alt="">
+        </a>
+    </nav>
+    <br>
+    <div class="tab-content">
+        <div class="tab-pane active" id="p1">WEB:</div>
+        <div class="tab-pane" id="p2">Mobile:</div>
+        <div class="tab-pane" id="p3">Data Science:</div>
+        <div class="tab-pane" id="p4">IoT:</div>
     </div>
-    <div class="wid_item">
-        <img class="wid_item_ref" src="public/img/icon link/mob.jpeg" alt=""> <span class="wid_item_name">Mobile</span>
-        <p>Desc</p>
-    </div>
-    <div class="wid_item">
-        <img class="wid_item_ref" src="public/img/icon link/ia.png" alt=""> <span class="wid_item_name">Data Science</span><img class="wid_item_ref" src="public/img/icon link/sds.png" alt="">
-        <p>Desc</p>
-    </div>
-    <div class="wid_item">
-        <img class="wid_item_ref" src="public/img/icon link/wgh.png" alt=""> <span class="wid_item_name">Internet des Objets</span>
-        <p>Desc</p>
-    </div>
+
+    <br>
     <hr>
-
-    <!--     <section id="tabs">
-        <div class="row">
-            <div class="col-xs-12 ">
-                <nav>
-                    <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-                        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-                        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-                        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
-                        <a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="false">About</a>
-                    </div>
-                </nav>
-                <div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                        consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
-                    </div>
-                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                        ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
-                    </div>
-                    <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-                        Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
-                    </div>
-                    <div class="tab-pane fade" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
-                        labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section> -->
+    <br>
 </div>
 
 
--- portfolio/resources/views/layouts/skeleton.blade.php
+++ portfolio/resources/views/layouts/skeleton.blade.php
@@ -45,6 +45,9 @@
         <div class="bas_page">
             <!--footer-->
         </div>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.0/umd/popper.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.min.js" integrity="sha512-8qmis31OQi6hIRgvkht0s6mCOittjMa9GMqtK9hes5iEQBQE/Ca6yGE5FsW36vyipGoWQswBj/QBm2JR086Rkw==" crossorigin="anonymous"></script>
         <!--<script src="../js/onglets.js"></script>-->
 </body>
 @show
